01dfe5f51ff1e44291e593862426e414876d45c0
金悦悦 <921386013@qq.com>
2017-07-05 14:38:19 +0800

    Initial Commit

---
+++ testTheme/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testTheme
+//
+//  Created by 伟运体育 on 2017/7/5.
+//  Copyright © 2017年 伟运体育. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testTheme/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testTheme
+//
+//  Created by 伟运体育 on 2017/7/5.
+//  Copyright © 2017年 伟运体育. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
